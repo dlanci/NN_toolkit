18f1d97ee376fe6e9388ca306f3c8d231fcbf37c
Davide <davide@visitor-31014450.dyndns.cern.ch>
2018-04-10 17:52:48 +0200

    cyclegan

--- architectures/cycleGAN.py
+++ architectures/cycleGAN.py
@@ -245,7 +245,7 @@ class cycleGAN(object):
             )
         )
         #cycle cost is low if cyclic images are similar to input images (in both sets)
-        g_cycle_cost = tf.reduce_mean(tf.abs(self.input_A-cycl_A)) + tf.reduce_mean(tf.abs(self.input_B-cycl_B))
+        g_cycle_cost = tf.reduce_mean(tf.squared_difference(self.input_A,cycl_A)) + tf.reduce_mean(tf.squared_difference(self.input_B,cycl_B))
 
 
         self.g_cost_A = g_cost_A + g_cycle_cost
